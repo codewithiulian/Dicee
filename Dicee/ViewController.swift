57d55c5c8b520a0a4e60f8bdc5ae922ef290aad7
Iulian Oana <iulian.andreioana@gmail.com>
2019-07-25 19:26:05 +0100

    Adding integers to hold the random values of the two dice

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var randomDiceIndex1 : Int = 0
+    var randomDiceIndex2 : Int = 0
+    
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
     
@@ -19,8 +22,13 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    // Callback for 'Roll it!' button click.
     @IBAction func rollButtonPressed(_ sender: UIButton){
+        // Two random ints (bound to 6) are generated.
+        randomDiceIndex1 = Int(arc4random_uniform(6))
+        randomDiceIndex2 = Int(arc4random_uniform(6))
         
+        print(randomDiceIndex1)
     }
 }
 
